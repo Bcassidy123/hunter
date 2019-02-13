16fe61e51f04f0f682e748a4f71f697cc7e9fe2e
Barry Cassidy <barrycassidy123@tutanota.com>
2019-02-14 06:31:57 +1100

    add default

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -15,6 +15,7 @@ include(hunter_user_error)
 
 # NOTE: no names with spaces!
 
+hunter_default_version(hello VERSION 0.1.0)
 hunter_default_version(ARM_NEON_2_x86_SSE VERSION 1.0.0-p0)
 hunter_default_version(AllTheFlopsThreads VERSION 0.1-p0)
 hunter_default_version(Android-Apk VERSION 1.1.14)
